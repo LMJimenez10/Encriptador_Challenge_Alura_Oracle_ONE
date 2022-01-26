cd644210adb356a5aececfc559d39833e6ebab18
luchojjimenez <lucianomjimenez@gmail.com>
2022-01-26 13:29:43 -0300

    Agregando función de verificar texto y otros detalles

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-
-/* Reglas de encriptación: 
-"e" es convertido para "enter" 
-"i" es convertido para "imes"
-"a" es convertido para "ai"
-"o" es convertido para "ober"
-"u" es convertido para "ufat"
-Solo letras minusculas
-No se permite acentuación de palabras 
-*/
-
-/* Reglas de desencriptación: 
-"enter" es convertido para "e" 
-"imes" es convertido para "i"
-"ai" es convertido para "a"
-"ober" es convertido para "o"
-"ufat" es convertido para "u"
-Solo letras minusculas
-No se permite acentuación de palabras   
-*/
-
 var mensaje = document.querySelector("#input-texto");
 var btnEncriptar = document.querySelector("#btn-encriptar");
 var btnDesencriptar = document.querySelector("#btn-desencriptar");
 var btnCopiar = document.querySelector("#btn-copiar");
-var rstFinal = document.querySelector("#msg");
-//resultadoFinal
+var mostrarFinal = document.querySelector("#msg");
+
 
 function encriptarReemplazo (letra) {
 	if (letra == 'a') {
@@ -45,6 +24,7 @@ function encriptarReemplazo (letra) {
     }
 }
 
+
 function desencriptarReemplazo(letra) {
 	if (letra == 'ai') {
 		letra = 'a';
@@ -64,34 +44,64 @@ function desencriptarReemplazo(letra) {
 	}
 }
 
+
 function encriptar(texto) {
 	var msgEncriptado = texto.replace(/a|e|i|o|u/g, encriptarReemplazo);
 	return msgEncriptado;
 }
 
-btnEncriptar.addEventListener('click',function (event) {
-	event.preventDefault();
-	var texto = mensaje.value;
-	var msgFinal = encriptar(texto);
-	// console.log(msgFinal);
-	rstFinal.value = msgFinal;
-})
 
 function desencriptar(texto) {
 	var msgDesencriptado = texto.replace(/ai|enter|imes|ober|ufat/g, desencriptarReemplazo);
 	return msgDesencriptado;
 }
 
-btnDesencriptar.addEventListener('click',function (event) {
+
+function verificarTexto(texto) {
+	var restriccion = /[^a-zñ\s]/g;
+	if (restriccion.test(texto)) {
+		alert("No se permiten mayúsculas, acentos ni caracteres especiales");
+		return false;
+	} else {
+		return true;
+	}
+}
+
+
+btnEncriptar.addEventListener('click',function (event) {
 	event.preventDefault();
 	var texto = mensaje.value;
-	var msgFinal2 = desencriptar(texto);
-	rstFinal.value = msgFinal2;
+	var verificado = verificarTexto(texto);
+	if (verificado == true) {
+		var msgFinal = encriptar(texto);
+		mostrarFinal.style.color = "";
+		mostrarFinal.value = msgFinal;
+	} else {
+		mostrarFinal.style.color = "red";
+		mostrarFinal.value = "Corrige e inténtalo de nuevo"
+	}
+});
+
+
+btnDesencriptar.addEventListener('click', function (event) {
+	event.preventDefault();
+	var texto = mensaje.value;
+	var verificado = verificarTexto(texto);
+	if (verificado == true) {
+		var msgFinal = desencriptar(texto);
+		mostrarFinal.style.color = "";
+		mostrarFinal.value = msgFinal;
+	} else {
+		mostrarFinal.style.color = "red";
+		mostrarFinal.value = "Corrige e inténtalo de nuevo"	
+	}
 })
+
 
 btnCopiar.addEventListener('click', function (event) {
 	event.preventDefault();
-	rstFinal.select();
-	navigator.clipboard.writeText(rstFinal.value); 
+	mostrarFinal.select();
+	navigator.clipboard.writeText(mostrarFinal.value); 
 	}
 )
+
